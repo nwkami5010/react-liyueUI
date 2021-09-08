09137e5fab62036f9d18c90354751b109cb16de3
nwkami <715190184@qq.com>
2021-09-09 00:12:28 +0800

    feat: support input icon demo

---
+++ src/input/demo/icon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// @ts-ignore
+import { Input } from 'react-liyueUI'
+import { CaretDownOutlined } from '@ant-design/icons'
+export default () => (
+  <div>
+    <Input icon={<CaretDownOutlined />} />
+  </div>
+)
