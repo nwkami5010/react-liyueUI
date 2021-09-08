d45ef3c9a91056afccb64093568164dda7cf6f14
nwkami <715190184@qq.com>
2021-09-08 10:20:29 +0800

    chore: confirm menu requirement and build folder

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { default as Button } from './Button/Button';
 // export { default as Switch } from './Switch/Switch';
 // export { default as Input } from './Input/Input';
 export { default as Alert } from './alert'
+export { default as Transition } from './transition'
+
+export { default as Menu } from './menu'
