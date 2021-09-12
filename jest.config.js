70c7bb6cf7358768f098859387359be28b4d53d8
nwkami <715190184@qq.com>
2021-09-12 22:23:35 +0800

    fix：尝试删除node-sass

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   roots: ['<rootDir>'],
-  setupFiles: ['<rootDir>/scripts/jest/setupEnzyme.ts'],
+  setupFiles: ['<rootDir>/scripts/jest/setup.ts'],
   transform: {
     '\\.(ts|tsx)$': 'ts-jest',
   },
